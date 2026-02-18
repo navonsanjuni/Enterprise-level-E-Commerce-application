10d05353012623e7e6c3c1e803f3d266fb571a94
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-02-18 13:22:00 +0530

    add exports for user management services and commands

--- modules/user-management/application/index.ts
+++ modules/user-management/application/index.ts
@@ -1,1 +1,9 @@
 // modules/user-management/application/index.ts
+export * from "./commands/index.js";
+export * from "./queries/index.js";
+export * from "./services/authentication.service.js";
+export * from "./services/user-profile.service.js";
+export * from "./services/address-management.service.js";
+export * from "./services/payment-method.service.js";
+export * from "./services/password-hasher.service.js";
+export * from "./services/verification.service.js";
