30bb771b59e72834de4e463b3e9afec47629ff64
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-02-18 13:20:53 +0530

    add query and handler exports for user management

--- modules/user-management/application/queries/index.ts
+++ modules/user-management/application/queries/index.ts
@@ -1,1 +1,18 @@
-// modules/user-management/application/queries/index.ts
+// Base interfaces (type-only exports)
+export type { IQuery, IQueryHandler } from "./get-user-profile.query.js";
+
+// Query interfaces (type-only exports)
+export type { GetUserProfileQuery } from "./get-user-profile.query.js";
+export type { ListAddressesQuery } from "./list-addresses.query.js";
+export type { ListPaymentMethodsQuery } from "./list-payment-methods.query.js";
+export type { GetUserByEmailQuery } from "./get-user-by-email.query.js";
+export type { GetUserDetailsQuery } from "./get-user-details.query.js";
+export type { ListUsersQuery } from "./list-user.query.js";
+
+// Query Handler classes (runtime exports)
+export { GetUserProfileHandler } from "./get-user-profile.query.js";
+export { ListAddressesHandler } from "./list-addresses.query.js";
+export { ListPaymentMethodsHandler } from "./list-payment-methods.query.js";
+export { GetUserByEmailHandler } from "./get-user-by-email.query.js";
+export { GetUserDetailsHandler } from "./get-user-details.query.js";
+export { ListUsersHandler } from "./list-user.query.js";
