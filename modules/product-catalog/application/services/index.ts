580eecb1a9d42178a9823187d4e47b50d22bc9a4
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-02-26 10:33:08 +0530

    add index file to export all management services for product catalog

---
+++ modules/product-catalog/application/services/index.ts
@@ -0,0 +1,14 @@
+// Export all services
+export { ProductManagementService } from "./product-management.service";
+export { CategoryManagementService } from "./category-management.service";
+export { MediaManagementService } from "./media-management.service";
+export { VariantManagementService } from "./variant-management.service";
+export { ProductSearchService } from "./product-search.service";
+export { SlugGeneratorService } from "./slug-generator.service";
+
+// Additional management services
+export { ProductMediaManagementService } from "./product-media-management.service";
+export { VariantMediaManagementService } from "./variant-media-management.service";
+export { ProductTagManagementService } from "./product-tag-management.service";
+export { SizeGuideManagementService } from "./size-guide-management.service";
+export { EditorialLookManagementService } from "./editorial-look-management.service";
