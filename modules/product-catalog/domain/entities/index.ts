e5c49d6653951c8dafb9f1767d6418c4f6858685
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-02-26 10:38:23 +0530

    add index file for product catalog entities with exports for core and association entities

---
+++ modules/product-catalog/domain/entities/index.ts
@@ -0,0 +1,81 @@
+export {
+  Product,
+  ProductStatus,
+  CreateProductData,
+  ProductData,
+  ProductRow,
+} from "./product.entity";
+export {
+  ProductVariant,
+  CreateVariantData,
+  VariantData,
+  VariantRow,
+} from "./product-variant.entity";
+export {
+  Category,
+  CreateCategoryData,
+  CategoryData,
+  CategoryRow,
+} from "./category.entity";
+export {
+  MediaAsset,
+  MediaAssetId,
+  CreateMediaAssetData,
+  MediaAssetData,
+  MediaAssetRow,
+} from "./media-asset.entity";
+export {
+  ProductTag,
+  ProductTagId,
+  CreateProductTagData,
+  ProductTagData,
+  ProductTagRow,
+} from "./product-tag.entity";
+export {
+  SizeGuide,
+  SizeGuideId,
+  Region,
+  CreateSizeGuideData,
+  SizeGuideData,
+  SizeGuideRow,
+} from "./size-guide.entity";
+export {
+  EditorialLook,
+  EditorialLookId,
+  CreateEditorialLookData,
+  EditorialLookData,
+  EditorialLookRow,
+} from "./editorial-look.entity";
+
+// Association entities
+export {
+  ProductCategory,
+  CreateProductCategoryData,
+  ProductCategoryData,
+  ProductCategoryRow,
+} from "./product-category.entity";
+export {
+  ProductMedia,
+  ProductMediaId,
+  CreateProductMediaData,
+  ProductMediaData,
+  ProductMediaRow,
+} from "./product-media.entity";
+export {
+  VariantMedia,
+  CreateVariantMediaData,
+  VariantMediaData,
+  VariantMediaRow,
+} from "./variant-media.entity";
+export {
+  ProductTagAssociation,
+  CreateProductTagAssociationData,
+  ProductTagAssociationData,
+  ProductTagAssociationRow,
+} from "./product-tag-association.entity";
+export {
+  EditorialLookProduct,
+  CreateEditorialLookProductData,
+  EditorialLookProductData,
+  EditorialLookProductRow,
+} from "./editorial-look-product.entity";
