9d0d0e40f93fdf81d96d0cea592e0da47db23b2c
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-02-26 17:28:54 +0530

    create index file for managing inventory entities and their properties

---
+++ modules/inventory-management/domain/entities/index.ts
@@ -0,0 +1,17 @@
+export { Stock, StockProps } from "./stock.entity";
+export {
+  InventoryTransaction,
+  InventoryTransactionProps,
+} from "./inventory-transaction.entity";
+export { StockAlert, StockAlertProps } from "./stock-alert.entity";
+export { Location, LocationProps, LocationAddress } from "./location.entity";
+export { Supplier, SupplierProps, SupplierContact } from "./supplier.entity";
+export { PurchaseOrder, PurchaseOrderProps } from "./purchase-order.entity";
+export {
+  PurchaseOrderItem,
+  PurchaseOrderItemProps,
+} from "./purchase-order-item.entity";
+export {
+  PickupReservation,
+  PickupReservationProps,
+} from "./pickup-reservation.entity";
