74b7f4830508ca76dc6efaedba8ec1374cad81e7
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-02-28 13:44:41 +0530

    remove unused validators for product catalog module

--- modules/product-catalog/infrastructure/http/validators/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./product.validator";
-export * from "./category.validator";
-export * from "./variant.validator";
-export * from "./media.validator";
-export * from "./search.validator";
