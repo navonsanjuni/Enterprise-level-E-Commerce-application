f238c3792810d0b4cc8ce1063c91f16d3b121032
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-03-03 12:46:22 +0530

    create PaymentIntentStatusEnum for managing payment intent statuses

---
+++ modules/payment-loyalty/domain/enums/payment-intent-status.enum.ts
@@ -0,0 +1,7 @@
+export enum PaymentIntentStatusEnum {
+  REQUIRES_ACTION = "requires_action",
+  AUTHORIZED = "authorized",
+  CAPTURED = "captured",
+  FAILED = "failed",
+  CANCELLED = "cancelled",
+}
