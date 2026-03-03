767992092ce5f1f884537f69bcd8d2ac42257e1b
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-03-03 13:46:08 +0530

    update query handler exports in cart module

--- modules/cart/application/queries/index.ts
+++ modules/cart/application/queries/index.ts
@@ -1,15 +1,17 @@
-// Query interfaces (type-only exports)
 export type { GetCartQuery } from "./get-cart.query";
-export type { GetActiveCartByUserQuery } from "./get-active-cart-by-user.query";
-export type { GetActiveCartByGuestTokenQuery } from "./get-active-cart-by-guest-token.query";
-export type { GetCartSummaryQuery } from "./get-cart-summary.query";
-export type { GetReservationsQuery } from "./get-reservations.query";
-export type { GetReservationByVariantQuery } from "./get-reservation-by-variant.query";
+export { GetCartHandler } from "./get-cart.handler";
 
-// Query Handler classes (runtime exports)
-export { GetCartHandler } from "./get-cart.query";
-export { GetActiveCartByUserHandler } from "./get-active-cart-by-user.query";
-export { GetActiveCartByGuestTokenHandler } from "./get-active-cart-by-guest-token.query";
-export { GetCartSummaryHandler } from "./get-cart-summary.query";
-export { GetReservationsHandler } from "./get-reservations.query";
-export { GetReservationByVariantHandler } from "./get-reservation-by-variant.query";
+export type { GetCartSummaryQuery } from "./get-cart-summary.query";
+export { GetCartSummaryHandler } from "./get-cart-summary.handler";
+
+export type { GetActiveCartByUserQuery } from "./get-active-cart-by-user.query";
+export { GetActiveCartByUserHandler } from "./get-active-cart-by-user.handler";
+
+export type { GetActiveCartByGuestTokenQuery } from "./get-active-cart-by-guest-token.query";
+export { GetActiveCartByGuestTokenHandler } from "./get-active-cart-by-guest-token.handler";
+
+export type { GetReservationsQuery } from "./get-reservations.query";
+export { GetReservationsHandler } from "./get-reservations.handler";
+
+export type { GetReservationByVariantQuery } from "./get-reservation-by-variant.query";
+export { GetReservationByVariantHandler } from "./get-reservation-by-variant.handler";
