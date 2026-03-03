4963db497d644b7b3a5e7a4f7607ad5d7a5d9223
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-03-03 16:17:40 +0530

    Refactor command exports in product catalog module for improved clarity and consistency

--- modules/product-catalog/application/commands/index.ts
+++ modules/product-catalog/application/commands/index.ts
@@ -1,34 +1,35 @@
-// Base class utilities (runtime export)
-export {
-  CommandResult,
-  ICommand,
-  ICommandHandler,
-} from "@/api/src/shared/application";
+export type { CreateProductCommand } from "./create-product.command";
+export { CreateProductHandler } from "./create-product.handler";
 
-// Export type-only interfaces
-export type { CreateProductCommand } from "./create-product.command.js";
-export type { UpdateProductCommand } from "./update-product.command.js";
-export type { DeleteProductCommand } from "./delete-product.command.js";
-export type { CreateProductVariantCommand } from "./create-product-variant.command.js";
-export type { UpdateProductVariantCommand } from "./update-product-variant.command.js";
-export type { DeleteProductVariantCommand } from "./delete-product-variant.command.js";
-export type { CreateCategoryCommand } from "./create-category.command.js";
-export type { UpdateCategoryCommand } from "./update-category.command.js";
-export type { DeleteCategoryCommand } from "./delete-category.command.js";
-export type { CreateProductTagCommand } from "./create-product-tag.command.js";
-export type { CreateMediaAssetCommand } from "./create-media-asset.command.js";
-export type { AssociateProductMediaCommand } from "./associate-product-media.command.js";
+export type { UpdateProductCommand } from "./update-product.command";
+export { UpdateProductHandler } from "./update-product.handler";
 
-// Export Handler classes (runtime exports)
-export { CreateProductHandler } from "./create-product.command.js";
-export { UpdateProductHandler } from "./update-product.command.js";
-export { DeleteProductHandler } from "./delete-product.command.js";
-export { CreateProductVariantHandler } from "./create-product-variant.command.js";
-export { UpdateProductVariantHandler } from "./update-product-variant.command.js";
-export { DeleteProductVariantHandler } from "./delete-product-variant.command.js";
-export { CreateCategoryHandler } from "./create-category.command.js";
-export { UpdateCategoryHandler } from "./update-category.command.js";
-export { DeleteCategoryHandler } from "./delete-category.command.js";
-export { CreateProductTagHandler } from "./create-product-tag.command.js";
-export { CreateMediaAssetHandler } from "./create-media-asset.command.js";
-export { AssociateProductMediaHandler } from "./associate-product-media.command.js";
+export type { DeleteProductCommand } from "./delete-product.command";
+export { DeleteProductHandler } from "./delete-product.handler";
+
+export type { CreateProductVariantCommand } from "./create-product-variant.command";
+export { CreateProductVariantHandler } from "./create-product-variant.handler";
+
+export type { UpdateProductVariantCommand } from "./update-product-variant.command";
+export { UpdateProductVariantHandler } from "./update-product-variant.handler";
+
+export type { DeleteProductVariantCommand } from "./delete-product-variant.command";
+export { DeleteProductVariantHandler } from "./delete-product-variant.handler";
+
+export type { CreateCategoryCommand } from "./create-category.command";
+export { CreateCategoryHandler } from "./create-category.handler";
+
+export type { UpdateCategoryCommand } from "./update-category.command";
+export { UpdateCategoryHandler } from "./update-category.handler";
+
+export type { DeleteCategoryCommand } from "./delete-category.command";
+export { DeleteCategoryHandler } from "./delete-category.handler";
+
+export type { CreateProductTagCommand } from "./create-product-tag.command";
+export { CreateProductTagHandler } from "./create-product-tag.handler";
+
+export type { CreateMediaAssetCommand } from "./create-media-asset.command";
+export { CreateMediaAssetHandler } from "./create-media-asset.handler";
+
+export type { AssociateProductMediaCommand } from "./associate-product-media.command";
+export { AssociateProductMediaHandler } from "./associate-product-media.handler";
