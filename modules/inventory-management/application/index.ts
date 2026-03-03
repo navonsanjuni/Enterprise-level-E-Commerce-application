2f16ef95ca7bb1c072c20527f5700249f6d2d645
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-03-03 20:35:20 +0530

    Add missing handler exports for various commands and queries in inventory management

--- modules/inventory-management/application/index.ts
+++ modules/inventory-management/application/index.ts
@@ -1,54 +1,97 @@
 // Stock Commands and Queries
 export * from "./commands/stock";
 export * from "./queries/stock/get-stock.query";
+export * from "./queries/stock/get-stock.handler";
 export * from "./queries/stock/get-stock-stats.query";
+export * from "./queries/stock/get-stock-stats.handler";
 export * from "./queries/stock/get-stock-by-variant.query";
+export * from "./queries/stock/get-stock-by-variant.handler";
 export * from "./queries/stock/get-total-available-stock.query";
+export * from "./queries/stock/get-total-available-stock.handler";
 export * from "./queries/stock/list-stocks.query";
+export * from "./queries/stock/list-stocks.handler";
 
 // Location Commands and Queries
 export * from "./commands/location/create-location.command";
+export * from "./commands/location/create-location.handler";
 export * from "./commands/location/update-location.command";
+export * from "./commands/location/update-location.handler";
 export * from "./commands/location/delete-location.command";
+export * from "./commands/location/delete-location.handler";
 export * from "./queries/location/get-location.query";
+export * from "./queries/location/get-location.handler";
 export * from "./queries/location/list-locations.query";
+export * from "./queries/location/list-locations.handler";
 
 // Supplier Commands and Queries
 export * from "./commands/supplier/create-supplier.command";
+export * from "./commands/supplier/create-supplier.handler";
 export * from "./commands/supplier/update-supplier.command";
+export * from "./commands/supplier/update-supplier.handler";
 export * from "./commands/supplier/delete-supplier.command";
+export * from "./commands/supplier/delete-supplier.handler";
 export * from "./queries/supplier/get-supplier.query";
+export * from "./queries/supplier/get-supplier.handler";
 export * from "./queries/supplier/list-suppliers.query";
+export * from "./queries/supplier/list-suppliers.handler";
 
 // Purchase Order Commands and Queries
 export * from "./commands/purchase-order/create-purchase-order.command";
+export * from "./commands/purchase-order/create-purchase-order.handler";
 export * from "./commands/purchase-order/add-po-item.command";
+export * from "./commands/purchase-order/add-po-item.handler";
 export * from "./commands/purchase-order/update-po-item.command";
+export * from "./commands/purchase-order/update-po-item.handler";
 export * from "./commands/purchase-order/remove-po-item.command";
+export * from "./commands/purchase-order/remove-po-item.handler";
 export * from "./commands/purchase-order/update-po-status.command";
+export * from "./commands/purchase-order/update-po-status.handler";
 export * from "./commands/purchase-order/receive-po-items.command";
+export * from "./commands/purchase-order/receive-po-items.handler";
 export * from "./commands/purchase-order/delete-purchase-order.command";
+export * from "./commands/purchase-order/delete-purchase-order.handler";
 export * from "./queries/purchase-order/get-purchase-order.query";
+export * from "./queries/purchase-order/get-purchase-order.handler";
 export * from "./queries/purchase-order/get-po-items.query";
+export * from "./queries/purchase-order/get-po-items.handler";
 export * from "./queries/purchase-order/list-purchase-orders.query";
+export * from "./queries/purchase-order/list-purchase-orders.handler";
 
 // Stock Alert Commands and Queries
 export * from "./commands/alert/create-stock-alert.command";
+export * from "./commands/alert/create-stock-alert.handler";
 export * from "./commands/alert/resolve-stock-alert.command";
+export * from "./commands/alert/resolve-stock-alert.handler";
+export * from "./commands/alert/delete-stock-alert.command";
+export * from "./commands/alert/delete-stock-alert.handler";
 export * from "./queries/alert/get-stock-alert.query";
+export * from "./queries/alert/get-stock-alert.handler";
 export * from "./queries/alert/get-active-alerts.query";
+export * from "./queries/alert/get-active-alerts.handler";
 export * from "./queries/alert/list-stock-alerts.query";
+export * from "./queries/alert/list-stock-alerts.handler";
 
 // Pickup Reservation Commands and Queries
 export * from "./commands/pickup-reservation/create-pickup-reservation.command";
+export * from "./commands/pickup-reservation/create-pickup-reservation.handler";
 export * from "./commands/pickup-reservation/cancel-pickup-reservation.command";
+export * from "./commands/pickup-reservation/cancel-pickup-reservation.handler";
+export * from "./commands/pickup-reservation/extend-reservation.command";
+export * from "./commands/pickup-reservation/extend-reservation.handler";
 export * from "./queries/pickup-reservation/get-pickup-reservation.query";
+export * from "./queries/pickup-reservation/get-pickup-reservation.handler";
 export * from "./queries/pickup-reservation/list-pickup-reservations.query";
+export * from "./queries/pickup-reservation/list-pickup-reservations.handler";
 
 // Transaction Queries
 export * from "./queries/transaction/get-transaction.query";
+export * from "./queries/transaction/get-transaction.handler";
 export * from "./queries/transaction/get-transactions-by-variant.query";
+export * from "./queries/transaction/get-transactions-by-variant.handler";
+export * from "./queries/transaction/get-transaction-history.query";
+export * from "./queries/transaction/get-transaction-history.handler";
 export * from "./queries/transaction/list-transactions.query";
+export * from "./queries/transaction/list-transactions.handler";
 
 // Services
 export * from "./services";
