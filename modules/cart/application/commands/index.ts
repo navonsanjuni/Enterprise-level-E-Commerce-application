78386fedb3bc8fded2dc90dcdca92a12c23a90b8
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-03-03 13:45:21 +0530

    correct handler imports in cart command index

--- modules/cart/application/commands/index.ts
+++ modules/cart/application/commands/index.ts
@@ -1,16 +1,23 @@
-﻿export type { AddToCartCommand } from "./add-to-cart.command";
-export { AddToCartHandler } from "./add-to-cart.command";
+export type { AddToCartCommand } from "./add-to-cart.command";
+export { AddToCartHandler } from "./add-to-cart.handler";
+
 export type { ClearCartCommand } from "./clear-cart.command";
-export { ClearCartHandler } from "./clear-cart.command";
+export { ClearCartHandler } from "./clear-cart.handler";
+
 export type { CreateGuestCartCommand } from "./create-guest-cart.command";
-export { CreateGuestCartHandler } from "./create-guest-cart.command";
+export { CreateGuestCartHandler } from "./create-guest-cart.handler";
+
 export type { CreateUserCartCommand } from "./create-user-cart.command";
-export { CreateUserCartHandler } from "./create-user-cart.command";
+export { CreateUserCartHandler } from "./create-user-cart.handler";
+
 export type { RemoveFromCartCommand } from "./remove-from-cart.command";
-export { RemoveFromCartHandler } from "./remove-from-cart.command";
+export { RemoveFromCartHandler } from "./remove-from-cart.handler";
+
 export type { TransferCartCommand } from "./transfer-cart.command";
-export { TransferCartHandler } from "./transfer-cart.command";
+export { TransferCartHandler } from "./transfer-cart.handler";
+
 export type { UpdateCartItemCommand } from "./update-cart-item.command";
-export { UpdateCartItemHandler } from "./update-cart-item.command";
+export { UpdateCartItemHandler } from "./update-cart-item.handler";
+
 export type { CreateReservationCommand } from "./create-reservation.command";
-export { CreateReservationHandler } from "./create-reservation.command";
+export { CreateReservationHandler } from "./create-reservation.handler";
