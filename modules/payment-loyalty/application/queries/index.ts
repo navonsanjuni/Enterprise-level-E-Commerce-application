012e47e87df21c2d8de01fee2b9a2665e71b6263
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-03-03 23:49:46 +0530

    Refactor query exports to use type exports for better type safety and clarity

--- modules/payment-loyalty/application/queries/index.ts
+++ modules/payment-loyalty/application/queries/index.ts
@@ -1,12 +1,25 @@
-// Barrel for queries
-export * from "./get-active-promotions.query";
-export * from "./get-bnpl-transactions.query";
-export * from "./get-gift-card-balance.query";
-export * from "./get-gift-card-transactions.query";
-export * from "./get-loyalty-account.query";
-export * from "./get-loyalty-programs.query";
-export * from "./get-loyalty-transactions.query";
-export * from "./get-payment-intent.query";
-export * from "./get-payment-transactions.query";
-export * from "./get-promotion-usage.query";
-export * from "./get-webhook-events.query";
+// Query interfaces
+export type { GetActivePromotionsQuery } from "./get-active-promotions.query";
+export type { GetBnplTransactionsQuery } from "./get-bnpl-transactions.query";
+export type { GetGiftCardBalanceQuery } from "./get-gift-card-balance.query";
+export type { GetGiftCardTransactionsQuery } from "./get-gift-card-transactions.query";
+export type { GetLoyaltyAccountQuery } from "./get-loyalty-account.query";
+export type { GetLoyaltyProgramsQuery } from "./get-loyalty-programs.query";
+export type { GetLoyaltyTransactionsQuery } from "./get-loyalty-transactions.query";
+export type { GetPaymentIntentQuery } from "./get-payment-intent.query";
+export type { GetPaymentTransactionsQuery } from "./get-payment-transactions.query";
+export type { GetPromotionUsageQuery } from "./get-promotion-usage.query";
+export type { GetWebhookEventsQuery } from "./get-webhook-events.query";
+
+// Query handlers
+export { GetActivePromotionsHandler } from "./get-active-promotions.handler";
+export { GetBnplTransactionsHandler } from "./get-bnpl-transactions.handler";
+export { GetGiftCardBalanceHandler } from "./get-gift-card-balance.handler";
+export { GetGiftCardTransactionsHandler } from "./get-gift-card-transactions.handler";
+export { GetLoyaltyAccountHandler } from "./get-loyalty-account.handler";
+export { GetLoyaltyProgramsHandler } from "./get-loyalty-programs.handler";
+export { GetLoyaltyTransactionsHandler } from "./get-loyalty-transactions.handler";
+export { GetPaymentIntentHandler } from "./get-payment-intent.handler";
+export { GetPaymentTransactionsHandler } from "./get-payment-transactions.handler";
+export { GetPromotionUsageHandler } from "./get-promotion-usage.handler";
+export { GetWebhookEventsHandler } from "./get-webhook-events.handler";
