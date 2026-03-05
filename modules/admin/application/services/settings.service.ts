4e7c9c78217ccfbebb9774793ac52ee8d98ce263
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-03-05 17:34:55 +0530

    Refactor SettingsService to utilize config for default shipping rates and prepare for future settings persistence

--- modules/admin/application/services/settings.service.ts
+++ modules/admin/application/services/settings.service.ts
@@ -3,12 +3,24 @@ export interface ShippingRates {
   suburbs: number;
 }
 
+export interface SettingsServiceConfig {
+  defaultShippingRates: ShippingRates;
+}
+
 export class SettingsService {
-  async getShippingRates(): Promise<ShippingRates> {
-    // TODO: Fetch from database/config
-    return {
-      colombo: 0,
-      suburbs: 0,
+  private readonly config: SettingsServiceConfig;
+
+  constructor(config?: Partial<SettingsServiceConfig>) {
+    this.config = {
+      defaultShippingRates: config?.defaultShippingRates ?? {
+        colombo: 0,
+        suburbs: 0,
+      },
     };
+  }
+
+  async getShippingRates(): Promise<ShippingRates> {
+    // TODO: Fetch from database when settings persistence is implemented
+    return this.config.defaultShippingRates;
   }
 }
