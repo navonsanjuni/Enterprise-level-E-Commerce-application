f4695487d580bd2dd13fa9a0d92486c20d66bc53
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-04-08 22:04:55 +0530

    Add index file to export AnalyticsEvent and related types

---
+++ modules/analytics/domain/entities/index.ts
@@ -0,0 +1,6 @@
+export { AnalyticsEvent } from './analytics-event.entity';
+export type {
+  AnalyticsEventMetadata,
+  CreateAnalyticsEventData,
+  ReconstituteAnalyticsEventData,
+} from './analytics-event.entity';
