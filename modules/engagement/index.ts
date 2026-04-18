d7d98d5da526190c5e7cf615e858d0ba2e1ca34a
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-04-19 02:42:56 +0530

    Add engagement module index file to export domain errors, enums, value objects, and application layer

---
+++ modules/engagement/index.ts
@@ -0,0 +1,19 @@
+// Errors
+export * from "./domain/errors";
+
+// Enums
+export * from "./domain/enums";
+
+// Identity Value Objects
+export {
+  WishlistId,
+  WishlistItemId,
+  ReminderId,
+  NotificationId,
+  ReviewId,
+  AppointmentId,
+  SubscriptionId,
+} from "./domain/value-objects";
+
+// Application layer
+export * from "./application";
