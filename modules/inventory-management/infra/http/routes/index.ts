8afc13818e76f1fa90207a5ae52f1f6d7e375d4e
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-04-22 02:29:18 +0530

    Refactor inventory management routes to use controllers instead of services for improved structure and maintainability

--- modules/inventory-management/infra/http/routes/index.ts
+++ modules/inventory-management/infra/http/routes/index.ts
@@ -1,70 +1,14 @@
 import { FastifyInstance } from "fastify";
-import { StockController } from "../controllers/stock.controller";
-import { LocationController } from "../controllers/location.controller";
-import { SupplierController } from "../controllers/supplier.controller";
-import { PurchaseOrderController } from "../controllers/purchase-order.controller";
-import { PurchaseOrderItemController } from "../controllers/purchase-order-item.controller";
-import { StockAlertController } from "../controllers/stock-alert.controller";
-import { PickupReservationController } from "../controllers/pickup-reservation.controller";
-import { InventoryTransactionController } from "../controllers/inventory-transaction.controller";
 import {
-  StockManagementService,
-  LocationManagementService,
-  SupplierManagementService,
-  PurchaseOrderManagementService,
-  StockAlertService,
-  PickupReservationService,
-  AddStockHandler,
-  AdjustStockHandler,
-  TransferStockHandler,
-  ReserveStockHandler,
-  FulfillReservationHandler,
-  SetStockThresholdsHandler,
-  GetStockHandler,
-  GetStockByVariantHandler,
-  GetStockStatsHandler,
-  GetTotalAvailableStockHandler,
-  ListStocksHandler,
-  GetLowStockItemsHandler,
-  GetOutOfStockItemsHandler,
-  CreateLocationHandler,
-  UpdateLocationHandler,
-  DeleteLocationHandler,
-  GetLocationHandler,
-  ListLocationsHandler,
-  CreateSupplierHandler,
-  UpdateSupplierHandler,
-  DeleteSupplierHandler,
-  GetSupplierHandler,
-  ListSuppliersHandler,
-  CreatePurchaseOrderHandler,
-  CreatePurchaseOrderWithItemsHandler,
-  AddPOItemHandler,
-  UpdatePOItemHandler,
-  RemovePOItemHandler,
-  UpdatePOStatusHandler,
-  UpdatePOEtaHandler,
-  ReceivePOItemsHandler,
-  DeletePurchaseOrderHandler,
-  GetPurchaseOrderHandler,
-  GetPOItemsHandler,
-  ListPurchaseOrdersHandler,
-  GetOverduePurchaseOrdersHandler,
-  GetPendingReceivalHandler,
-  CreateStockAlertHandler,
-  ResolveStockAlertHandler,
-  DeleteStockAlertHandler,
-  GetStockAlertHandler,
-  GetActiveAlertsHandler,
-  ListStockAlertsHandler,
-  CreatePickupReservationHandler,
-  CancelPickupReservationHandler,
-  GetPickupReservationHandler,
-  ListPickupReservationsHandler,
-  GetTransactionHandler,
-  ListTransactionsHandler,
-  GetTransactionsByVariantHandler,
-} from "../../../application";
+  StockController,
+  LocationController,
+  SupplierController,
+  PurchaseOrderController,
+  PurchaseOrderItemController,
+  StockAlertController,
+  PickupReservationController,
+  InventoryTransactionController,
+} from "../controllers";
 import { stockRoutes } from "./stock.routes";
 import { locationRoutes } from "./location.routes";
 import { supplierRoutes } from "./supplier.routes";
@@ -75,114 +19,30 @@ import { pickupReservationRoutes } from "./pickup-reservation.routes";
 import { inventoryTransactionRoutes } from "./inventory-transaction.routes";
 
 export interface InventoryManagementRouteServices {
-  stockService: StockManagementService;
-  locationService: LocationManagementService;
-  supplierService: SupplierManagementService;
-  poService: PurchaseOrderManagementService;
-  alertService: StockAlertService;
-  reservationService: PickupReservationService;
+  stockController: StockController;
+  locationController: LocationController;
+  supplierController: SupplierController;
+  poController: PurchaseOrderController;
+  poItemController: PurchaseOrderItemController;
+  alertController: StockAlertController;
+  pickupReservationController: PickupReservationController;
+  inventoryTransactionController: InventoryTransactionController;
 }
 
 export async function registerInventoryManagementRoutes(
   fastify: FastifyInstance,
-  services: InventoryManagementRouteServices,
+  controllers: InventoryManagementRouteServices,
 ): Promise<void> {
-  // Stock handlers
-  const stockController = new StockController(
-    new AddStockHandler(services.stockService),
-    new AdjustStockHandler(services.stockService),
-    new TransferStockHandler(services.stockService),
-    new ReserveStockHandler(services.stockService),
-    new FulfillReservationHandler(services.stockService),
-    new SetStockThresholdsHandler(services.stockService),
-    new GetStockHandler(services.stockService),
-    new GetStockByVariantHandler(services.stockService),
-    new GetStockStatsHandler(services.stockService),
-    new GetTotalAvailableStockHandler(services.stockService),
-    new ListStocksHandler(services.stockService),
-    new GetLowStockItemsHandler(services.stockService),
-    new GetOutOfStockItemsHandler(services.stockService),
-  );
-
-  // Location handlers
-  const locationController = new LocationController(
-    new CreateLocationHandler(services.locationService),
-    new UpdateLocationHandler(services.locationService),
-    new DeleteLocationHandler(services.locationService),
-    new GetLocationHandler(services.locationService),
-    new ListLocationsHandler(services.locationService),
-  );
-
-  // Supplier handlers
-  const supplierController = new SupplierController(
-    new CreateSupplierHandler(services.supplierService),
-    new UpdateSupplierHandler(services.supplierService),
-    new DeleteSupplierHandler(services.supplierService),
-    new GetSupplierHandler(services.supplierService),
-    new ListSuppliersHandler(services.supplierService),
-  );
-
-  // Purchase order handlers
-  const poController = new PurchaseOrderController(
-    new CreatePurchaseOrderHandler(services.poService),
-    new CreatePurchaseOrderWithItemsHandler(services.poService),
-    new AddPOItemHandler(services.poService),
-    new UpdatePOItemHandler(services.poService),
-    new RemovePOItemHandler(services.poService),
-    new UpdatePOStatusHandler(services.poService),
-    new ReceivePOItemsHandler(services.poService),
-    new DeletePurchaseOrderHandler(services.poService),
-    new GetPurchaseOrderHandler(services.poService),
-    new GetPOItemsHandler(services.poService),
-    new ListPurchaseOrdersHandler(services.poService),
-    new GetOverduePurchaseOrdersHandler(services.poService),
-    new GetPendingReceivalHandler(services.poService),
-    new UpdatePOEtaHandler(services.poService),
-  );
-
-  // Purchase order item handlers (reuse PO service handlers)
-  const poItemController = new PurchaseOrderItemController(
-    new AddPOItemHandler(services.poService),
-    new UpdatePOItemHandler(services.poService),
-    new RemovePOItemHandler(services.poService),
-    new GetPOItemsHandler(services.poService),
-  );
-
-  // Stock alert handlers
-  const alertController = new StockAlertController(
-    new CreateStockAlertHandler(services.alertService),
-    new ResolveStockAlertHandler(services.alertService),
-    new DeleteStockAlertHandler(services.alertService),
-    new GetStockAlertHandler(services.alertService),
-    new GetActiveAlertsHandler(services.alertService),
-    new ListStockAlertsHandler(services.alertService),
-  );
-
-  // Pickup reservation handlers
-  const reservationController = new PickupReservationController(
-    new CreatePickupReservationHandler(services.reservationService),
-    new CancelPickupReservationHandler(services.reservationService),
-    new GetPickupReservationHandler(services.reservationService),
-    new ListPickupReservationsHandler(services.reservationService),
-  );
-
-  // Inventory transaction handlers (use stock service)
-  const transactionController = new InventoryTransactionController(
-    new GetTransactionsByVariantHandler(services.stockService),
-    new ListTransactionsHandler(services.stockService),
-    new GetTransactionHandler(services.stockService),
-  );
-
   await fastify.register(
     async (instance) => {
-      await stockRoutes(instance, stockController);
-      await locationRoutes(instance, locationController);
-      await supplierRoutes(instance, supplierController);
-      await purchaseOrderRoutes(instance, poController);
-      await purchaseOrderItemRoutes(instance, poItemController);
-      await stockAlertRoutes(instance, alertController);
-      await pickupReservationRoutes(instance, reservationController);
-      await inventoryTransactionRoutes(instance, transactionController);
+      await stockRoutes(instance, controllers.stockController);
+      await locationRoutes(instance, controllers.locationController);
+      await supplierRoutes(instance, controllers.supplierController);
+      await purchaseOrderRoutes(instance, controllers.poController);
+      await purchaseOrderItemRoutes(instance, controllers.poItemController);
+      await stockAlertRoutes(instance, controllers.alertController);
+      await pickupReservationRoutes(instance, controllers.pickupReservationController);
+      await inventoryTransactionRoutes(instance, controllers.inventoryTransactionController);
     },
     { prefix: "/api/v1" },
   );
