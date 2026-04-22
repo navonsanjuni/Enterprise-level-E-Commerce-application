df0128199f4dcebd42cfcef30861e55bf567f42f
Tasheen Darshika <darshikatasheen99@gmail.com>
2026-04-22 10:32:58 +0530

    Remove GetUserByEmailHandler export from user management queries

--- modules/user-management/application/queries/index.ts
+++ modules/user-management/application/queries/index.ts
@@ -1,6 +1,5 @@
 export { GetUserProfileHandler } from "./get-user-profile.query";
 export { ListAddressesHandler } from "./list-addresses.query";
 export { ListPaymentMethodsHandler } from "./list-payment-methods.query";
-export { GetUserByEmailHandler } from "./get-user-by-email.query";
 export { GetUserDetailsHandler } from "./get-user-details.query";
 export { ListUsersHandler } from "./list-user.query";
