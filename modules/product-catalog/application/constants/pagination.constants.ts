28dd7bcbee0b806bbc687d2aad8352cb31487619
navonsanjuni <navonsanjuni178@gmail.com>
2026-04-26 09:39:27 +0530

    feat: Add pagination constants for managing page and suggestion limits

---
+++ modules/product-catalog/application/constants/pagination.constants.ts
@@ -0,0 +1,9 @@
+export const MIN_PAGE = 1;
+export const MIN_LIMIT = 1;
+
+export const DEFAULT_PAGE_SIZE = 20;
+export const MAX_PAGE_SIZE = 100;
+
+export const DEFAULT_SUGGESTIONS_LIMIT = 10;
+export const MAX_SUGGESTIONS_LIMIT = 50;
+ 
