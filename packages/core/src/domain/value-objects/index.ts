eb5d302a294d78ceb54c3d55bfa3e0d14a1ef7d9
navonsanjuni <navonsanjuni178@gmail.com>
2026-04-26 09:36:34 +0530

    refactor: Introduce currency value object and constants for enhanced currency management

--- packages/core/src/domain/value-objects/index.ts
+++ packages/core/src/domain/value-objects/index.ts
@@ -1,1 +1,10 @@
 export * from './uuid-id.base';
+export { Currency } from './currency.vo';
+export {
+  SUPPORTED_CURRENCIES,
+  CRYPTO_CURRENCIES,
+  CURRENCY_NAMES,
+  CURRENCY_SYMBOLS,
+  CURRENCY_REGIONS,
+  DEFAULT_CURRENCY,
+} from './currency.constants';
