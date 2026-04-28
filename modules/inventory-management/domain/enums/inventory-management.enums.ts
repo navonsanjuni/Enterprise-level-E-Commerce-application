edbb4bd3670372f20a5ee746612e4aed86d72c6f
navonsanjuni <navonsanjuni178@gmail.com>
2026-04-28 18:17:36 +0530

    refactor: remove unused enums, interfaces, and value objects from inventory and order management modules

--- modules/inventory-management/domain/enums/inventory-management.enums.ts
+++
@@ -1,5 +0,0 @@
-export { AlertType } from "../value-objects/alert-type.vo";
-export { LocationType } from "../value-objects/location-type.vo";
-export { PurchaseOrderStatus } from "../value-objects/purchase-order-status.vo";
-export { ReservationStatus } from "../value-objects/reservation-status.vo";
-export { TransactionReason } from "../value-objects/transaction-reason.vo";
